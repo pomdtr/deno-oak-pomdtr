698dd959f07ff1d9d18b9cb75df4488968e7bb46
Achille Lacoin <achille.lacoin@gmail.com>
2023-06-06 21:06:00 +0200

    remove unused params

--- main.ts
+++ main.ts
@@ -3,44 +3,27 @@ import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import data from "./data.json" assert { type: "json" };
 
 const router = new Router();
-router
-  .get("/", (context) => {
-    context.response.body = JSON.stringify(
-      {
-        type: "list",
-        items: data.map((dinosaur) => ({
-          title: dinosaur.name,
-          subtitle: dinosaur.description,
-          actions: [
-            { type: "copy", title: "Copy Name", text: dinosaur.name },
-            {
-              type: "copy",
-              title: "Copy Description",
-              text: dinosaur.description,
-            },
-          ],
-        })),
-      },
-      null,
-      2
-    );
-  })
-  .get("/api", (context) => {
-    context.response.body = data;
-  })
-  .get("/api/:dinosaur", (context) => {
-    if (context?.params?.dinosaur) {
-      const found = data.find(
-        (item) =>
-          item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
-      );
-      if (found) {
-        context.response.body = found;
-      } else {
-        context.response.body = "No dinosaurs found.";
-      }
-    }
-  });
+router.get("/", (context) => {
+  context.response.body = JSON.stringify(
+    {
+      type: "list",
+      items: data.map((dinosaur) => ({
+        title: dinosaur.name,
+        subtitle: dinosaur.description,
+        actions: [
+          { type: "copy", title: "Copy Name", text: dinosaur.name },
+          {
+            type: "copy",
+            title: "Copy Description",
+            text: dinosaur.description,
+          },
+        ],
+      })),
+    },
+    null,
+    2
+  );
+});
 
 const app = new Application();
 app.use(oakCors()); // Enable CORS for All Routes
