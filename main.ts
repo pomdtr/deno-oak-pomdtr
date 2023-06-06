1dc60b8207e44c8cb7f1a89d603374d1bff1ed7e
pomdtr <achille@deta.sh>
2023-06-07 00:30:55 +0200

    add subaction

--- main.ts
+++ main.ts
@@ -3,27 +3,68 @@ import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import data from "./data.json" assert { type: "json" };
 
 const router = new Router();
-router.get("/", (context) => {
-  context.response.body = JSON.stringify(
-    {
-      type: "list",
-      items: data.map((dinosaur) => ({
-        title: dinosaur.name,
-        subtitle: dinosaur.description,
+router
+  .get("/", (context) => {
+    context.response.body = JSON.stringify(
+      {
+        type: "list",
+        items: data.map((dinosaur) => ({
+          title: dinosaur.name,
+          subtitle: dinosaur.description,
+          actions: [
+            {
+              type: "push",
+              page: {
+                url: `/dinosaur/${dinosaur.name}`,
+              },
+            },
+            { type: "copy", title: "Copy Name", text: dinosaur.name },
+            {
+              type: "copy",
+              title: "Copy Description",
+              text: dinosaur.description,
+            },
+          ],
+        })),
+      },
+      null,
+      2
+    );
+  })
+  .get("/dinosaur/:name", (context) => {
+    const { name } = context.params;
+    const dinosaur = data.find((dinosaur) => dinosaur.name === name);
+    if (!dinosaur) {
+      context.response.status = 404;
+      context.response.body = JSON.stringify(
+        { error: "Dinosaur not found" },
+        null,
+        2
+      );
+      return;
+    }
+
+    context.response.body = JSON.stringify(
+      {
+        type: "detail",
+        preview: dinosaur.description,
         actions: [
-          { type: "copy", title: "Copy Name", text: dinosaur.name },
+          {
+            type: "copy",
+            title: "Copy Name",
+            text: dinosaur.name,
+          },
           {
             type: "copy",
             title: "Copy Description",
             text: dinosaur.description,
           },
         ],
-      })),
-    },
-    null,
-    2
-  );
-});
+      },
+      null,
+      2
+    );
+  });
 
 const app = new Application();
 app.use(oakCors()); // Enable CORS for All Routes
