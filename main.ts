c91dbc1ab6585df6260099473e3dc9dcd13d1351
pomdtr <achille@deta.sh>
2023-06-07 10:59:41 +0200

    add helper functions

--- main.ts
+++ main.ts
@@ -1,32 +1,34 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import { Page } from "https://deno.land/x/sunbeam@v1.0.0-rc.12/index.d.ts";
+import * as sunbeam from "https://deno.land/x/sunbeam@v1.0.0-rc.14/index.ts";
 import data from "./data.json" assert { type: "json" };
 
 const router = new Router();
 router
   .get("/", (context) => {
-    context.response.body = {
+    context.response.body = sunbeam.List({
       type: "list",
-      items: data.map((dinosaur) => ({
-        title: dinosaur.name,
-        subtitle: dinosaur.description,
-        actions: [
-          {
-            type: "push",
-            page: {
-              url: `/dinosaur/${dinosaur.name}`,
+      items: data.map((dinosaur) =>
+        sunbeam.Item({
+          title: dinosaur.name,
+          subtitle: dinosaur.description,
+          actions: [
+            {
+              type: "push",
+              page: {
+                url: `/dinosaur/${dinosaur.name}`,
+              },
             },
-          },
-          { type: "copy", title: "Copy Name", text: dinosaur.name },
-          {
-            type: "copy",
-            title: "Copy Description",
-            text: dinosaur.description,
-          },
-        ],
-      })),
-    } as Page;
+            { type: "copy", title: "Copy Name", text: dinosaur.name },
+            {
+              type: "copy",
+              title: "Copy Description",
+              text: dinosaur.description,
+            },
+          ],
+        })
+      ),
+    });
   })
   .get("/dinosaur/:name", (context) => {
     const { name } = context.params;
@@ -38,7 +40,7 @@ router
       return;
     }
 
-    context.response.body = {
+    context.response.body = sunbeam.Detail({
       type: "detail",
       preview: dinosaur.description,
       actions: [
@@ -53,7 +55,7 @@ router
           text: dinosaur.description,
         },
       ],
-    } as Page;
+    });
   });
 
 const app = new Application();
